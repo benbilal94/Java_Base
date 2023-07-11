304ec386e523d2100928eae0d145420e254c180a
Stagiaire <bilal-b@live.be>
2023-07-11 15:58:09 +0200

    commit 11/07/2023 fi de journée

--- src/main/java/be/bstorm/demo/DemTableaux.java
+++ src/main/java/be/bstorm/demo/DemTableaux.java
@@ -2,13 +2,42 @@ package be.bstorm.demo;
 
 public class DemTableaux {
     public static void main(String[] args) {
-        String [] strings = new String[3];
-        strings[0] = "Piere";
-        strings[1] = "Paul";
-        strings[2] = "Jaques";
+        String [] name = new String[3];
+        name[0] = "Piere";
+        name[1] = "Paul";
+        name[2] = "Jaques";
 
-        for (int i = 0; i < strings.length; i++) {
-            System.out.println("Bonjour " + strings[i]);
+        for (int i = 0; i < name.length; i++) {
+            System.out.println("Bonjour " + name[i]);
         }
+
+        String[][] strings2D = new String [5][3];
+
+        int[][] ints2D = new int[3][3];
+
+//        ints2D[0][0] = 1;
+//        ints2D[0][1] = 2;
+//        ...
+
+        int cpt = 1;
+        for (int i = 0; i < ints2D.length; i++ ){
+
+            for (int j = 0; j < ints2D[i].length; j++) {
+
+               ints2D[i][j] = cpt++;
+            }
+
+        }
+
+        for (int i = 0; i < ints2D.length; i++ ){
+
+            for (int j = 0; j < ints2D[i].length; j++) {
+
+                System.out.print(ints2D[i][j] + " | ");
+            }
+            System.out.println();
+
+        }
+
     }
 }
